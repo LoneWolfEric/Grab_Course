e2c58633d9396ec1c0b6f11194f9fa3c7b4bc322
LoneWolfEric <871855626@qq.com>
2018-06-26 02:41:18 +0930

    模块化

--- grab_course.py
+++ grab_course.py
@@ -5,28 +5,36 @@ import re
 user_login = requests.session()
 
 def login_in():
+    while True:
+        zjh = str(input('请输入你的学号：'))
+        mm = str(input('请输入你的教务处密码：'))
 
-    zjh = input('请输入你的学号：')
-    mm = input('请输入你的教务处密码：')
+        url = 'http://zhjw.scu.edu.cn/loginAction.do'
 
-    url = 'http://zhjw.scu.edu.cn/loginAction.do'
+        headers = {
+            'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36',
+            'Cookie':'JSESSIONID=bcdyFntur0YE6-Ttl03qw',
+            'Host':'zhjw.scu.edu.cn',
+            'Origin':'http://zhjw.scu.edu.cn',
+            'Referer':'http://zhjw.scu.edu.cn/login.jsp'
+        }
 
-    headers = {
-        'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36',
-        'Cookie':'JSESSIONID=bcdyFntur0YE6-Ttl03qw',
-        'Host':'zhjw.scu.edu.cn',
-        'Origin':'http://zhjw.scu.edu.cn',
-        'Referer':'http://zhjw.scu.edu.cn/login.jsp'
-    }
+        data = {
+            'zjh': zjh,
+            'mm': mm
+        }
 
-    data = {
-        'zjh': '2016141442100',
-        'mm': '081318'
-    }
+        response = user_login.post(url, data = data, headers = headers)          # 登录教务处
 
-    user_login.post(url, data = data, headers = headers)        #登录教务处
-    print('登录成功')
 
+        isError  = re.findall(r'<td class="errorTop">', response.content.decode('gbk'))
+
+        if isError:                             # 判断账号密码是否正确
+            print('账号或者密码错误，请重新输入！')
+        else:
+            print('登录成功')
+            break
+            
 def show_user_info():
     url = 'http://zhjw.scu.edu.cn/menu/s_top.jsp'
 
@@ -42,7 +50,7 @@ def show_user_info():
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36'
     }
 
-    user_info = user_login.get(url, headers = name_headers)     #获取个人信息
+    user_info = user_login.get(url, headers = name_headers)     # 获取个人信息
     html = user_info.content.decode('gbk')
 
     user_name = re.findall(r'欢迎光临&nbsp;.{0,6}&nbsp;', html)
@@ -50,7 +58,7 @@ def show_user_info():
     num = user_name.rindex('&nbsp;')
     user_name = user_name[10: num]
 
-    print('你好，%s!' %(user_name))
+    print('你好，%s!' %(user_name))                             # 显示个人信息
 
 
 
